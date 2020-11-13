4e5e7c0e53f0d7aae35d8e7eebf1ec09119b131c
dennis7031 <dennisorellana7941@gmail.com>
2020-11-13 15:47:26 -0400

    update

---
+++ Practicas/Practica3Web/Dennis/PracticaTres/js/main.js
@@ -0,0 +1,32 @@
+/*price range*/
+
+if ($.fn.slider) {
+    $('#sl2').slider();
+}
+
+var RGBChange = function () {
+    $('#RGB').css('background', 'rgb(' + r.getValue() + ',' + g.getValue() + ',' + b.getValue() + ')')
+};
+
+/*scroll to top*/
+
+$(document).ready(function () {
+    $(function () {
+        $.scrollUp({
+            scrollName: 'scrollUp', // Element ID
+            scrollDistance: 300, 
+            scrollFrom: 'top', 
+            scrollSpeed: 300, 
+            easingType: 'linear', 
+            animation: 'fade', 
+            animationSpeed: 200, 
+            scrollTrigger: false, 
+            
+            scrollText: '<i class="fa fa-angle-up"></i>', 
+            scrollTitle: false, 
+            scrollImg: false, 
+            activeOverlay: false, 
+            zIndex: 2147483647 
+        });
+    });
+});
